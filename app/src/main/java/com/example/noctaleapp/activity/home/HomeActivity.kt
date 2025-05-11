5500c02c865411d082b39e1762210488b5460c52
Ryiong <viethung1209.work@gmail.com>
2025-05-11 23:25:34 +0700

    Refactor: Change theme and bottom navigation

--- app/src/main/java/com/example/noctaleapp/activity/home/HomeActivity.kt
+++ app/src/main/java/com/example/noctaleapp/activity/home/HomeActivity.kt
@@ -1,6 +1,7 @@
 package com.example.noctaleapp.activity.home
 
 import android.os.Bundle
+import android.view.WindowManager
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -10,6 +11,10 @@ import com.example.noctaleapp.R
 class HomeActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        getWindow().setFlags(
+            WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS,
+            WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS
+        )
         enableEdgeToEdge()
         setContentView(R.layout.activity_home)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
