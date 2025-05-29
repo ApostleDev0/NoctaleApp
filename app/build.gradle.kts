3342bbd0e144b1ea146f5b7a44f49c1d75a83ede
Ryiong <viethung1209.work@gmail.com>
2025-05-29 12:59:04 +0700

    Feat: Integrate Firebase Crashlytics
    This commit integrates Firebase Crashlytics for crash reporting and analytics.
    
    - Added the Google Services plugin to the project and app-level `build.gradle.kts` files.
    - Included the Firebase Bill of Materials (BOM) and Firebase Analytics dependencies in the app-level `build.gradle.kts`.
    - Added the `google-services.json` configuration file.
    - Updated `.idea/appInsightsSettings.xml` with Firebase project details for Crashlytics integration in Android Studio.
    - Corrected a typo in `ResetPasswordActivity.kt` class name from `esetPasswordActivity` to `ResetPasswordActivity`.
    - Added XML declaration to `.idea/misc.xml`.

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,6 +1,7 @@
 plugins {
     alias(libs.plugins.android.application)
     alias(libs.plugins.kotlin.android)
+    id("com.google.gms.google-services")
 }
 
 android {
@@ -56,4 +57,6 @@ dependencies {
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
     implementation("com.github.bumptech.glide:glide:4.16.0")
+    implementation(platform("com.google.firebase:firebase-bom:33.14.0"))
+    implementation("com.google.firebase:firebase-analytics")
 }
--- app/src/main/java/com/example/noctaleapp/ui/ResetPasswordActivity.kt
+++ app/src/main/java/com/example/noctaleapp/ui/ResetPasswordActivity.kt
@@ -7,7 +7,7 @@ import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import com.example.noctaleapp.R
 
-class esetPasswordActivity : AppCompatActivity() {
+class ResetPasswordActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
