9cb7a171adc714f317a9e951daee7121a5a21b53
Dev_Num0 <hoangnguyen030603@gmail.com>
2025-06-09 19:13:34 +0700

    Remove findviewby(R.id.root) Using View Binding for Login, Signup, ResetPassword Code logic when run app will start with login, from login can open layout signup and resetpassword

--- app/src/main/java/com/example/noctaleapp/ui/LoginActivity.kt
+++ app/src/main/java/com/example/noctaleapp/ui/LoginActivity.kt
@@ -1,21 +1,42 @@
 package com.example.noctaleapp.ui
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
-import com.example.noctaleapp.R
+import com.example.noctaleapp.databinding.ActivityLoginBinding
+import com.example.noctaleapp.ui.resetpassword.ResetPasswordActivity
 
 class LoginActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityLoginBinding // khai báo biến binding cho login
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_login)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+
+        // Khởi tạo binding
+        binding = ActivityLoginBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        ViewCompat.setOnApplyWindowInsetsListener(binding.loginRootLayout) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
+        }
+
+        // Xử lý sự kiện cho nút "Đăng ký ngay"
+        binding.txtViewSignupNow.setOnClickListener {
+            val intent = Intent(this, SignupActivity::class.java)
+            startActivity(intent)
+        }
+
+        // Xử lý sự kiện click cho "Quên mật khẩu"
+        binding.txtViewForgetPasswordLogin.setOnClickListener {
+            val intent = Intent(this, ResetPasswordActivity::class.java)
+            startActivity(intent)
         }
     }
 }
--- app/src/main/java/com/example/noctaleapp/ui/resetpassword/ResetPasswordActivity.kt
+++ app/src/main/java/com/example/noctaleapp/ui/resetpassword/ResetPasswordActivity.kt
@@ -5,14 +5,22 @@ import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
-import com.example.noctaleapp.R
+import com.example.noctaleapp.databinding.ActivityResetPasswordBinding
 
 class ResetPasswordActivity : AppCompatActivity() {
+
+    // khai báo biến binding cho Reset Pass
+    private lateinit var binding: ActivityResetPasswordBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_reset_password)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+
+        // khởi tạo binding
+        binding = ActivityResetPasswordBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        ViewCompat.setOnApplyWindowInsetsListener(binding.layoutResetPasswordContainer) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
