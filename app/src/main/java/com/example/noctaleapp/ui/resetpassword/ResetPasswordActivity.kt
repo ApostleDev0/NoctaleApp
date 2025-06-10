78bf037ae1d2b9d26ac030a1d10a81cf2dc92177
Dev_Num0 <hoangnguyen030603@gmail.com>
2025-06-10 18:17:27 +0700

    Update stream for ResetPassword, App now can target layout ResetPassword and show Fragment ForgotPassword Add Interface and logic for activity "back" to return from Fragment ForgotPassword to Login Layout

--- app/src/main/java/com/example/noctaleapp/ui/resetpassword/ResetPasswordActivity.kt
+++ app/src/main/java/com/example/noctaleapp/ui/resetpassword/ResetPasswordActivity.kt
@@ -5,9 +5,10 @@ import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.example.noctaleapp.R
 import com.example.noctaleapp.databinding.ActivityResetPasswordBinding
 
-class ResetPasswordActivity : AppCompatActivity() {
+class ResetPasswordActivity : AppCompatActivity(), ForgotPasswordFragment.OnForgotPasswordListener {
 
     // khai báo biến binding cho Reset Pass
     private lateinit var binding: ActivityResetPasswordBinding
@@ -25,5 +26,21 @@ class ResetPasswordActivity : AppCompatActivity() {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+
+        // Fragments
+        // Điều kiện: chỉ thêm fragment khi activity được tạo lần đầu
+        // tránh các fragment chồng lên nhau khi xoay màn hình
+        if (savedInstanceState == null) {
+            // tạo instance của fragment muốn hiển thị
+            val forgotPasswordFragment = ForgotPasswordFragment()
+
+            // gắn transaction cho fragment
+            supportFragmentManager.beginTransaction().replace(R.id.layout_reset_password_container, forgotPasswordFragment).commit()
+        }
     }
+
+    override fun onBackPassword() {
+        finish()
+    }
+
 }
